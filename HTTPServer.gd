a89cf4125f76e6a12f23f05ebbd751f51f09a764
Marzin <70151472+Marzin-bot@users.noreply.github.com>
2022-05-25 16:59:45 +0200

    Update HTTPServer.gd

--- HTTPServer.gd
+++ HTTPServer.gd
@@ -1,12 +1,14 @@
 class_name HTTPServer
 
-extends Reference
+extends RefCounted
 
 ##Hyper-text transfer protocol server.
 ##
 ##@desc:
 ##	Hyper-text transfer protocol server.
 ##	Used to serve files and other data, communicate, process requests from HTTP client (sometimes called "user agents") among other use cases.
+##
+##@tutorial: tutorial.html
 ##
 
 const COMPRESSION_NAME_MODE: Dictionary = {
@@ -16,39 +18,40 @@ const COMPRESSION_NAME_MODE: Dictionary = {
 	"fastlz": File.COMPRESSION_FASTLZ
 }
 
-const DATETIM_NAME = {
-	day = {
-		1: "Mon",
-		2: "Tue",
-		3: "Wed",
-		4: "Thu",
-		5: "Fri",
-		6: "Sat",
-		0: "Sun"
-	},
-	month = {
-		1: "Jan",
-		2: "Feb",
-		3: "Mar",
-		4: "Apr",
-		5: "May",
-		6: "Jun",
-		7: "Jul",
-		8: "Aug",
-		9: "Sep",
-		10: "Oct",
-		11: "Nov",
-		12: "Dec"
-	}
+const DAYS_NAME: Dictionary = {
+	1: "Mon",
+	2: "Tue",
+	3: "Wed",
+	4: "Thu",
+	5: "Fri",
+	6: "Sat",
+	0: "Sun"
 }
+
+const MONTHS_NAME: Dictionary = {
+	1: "Jan",
+	2: "Feb",
+	3: "Mar",
+	4: "Apr",
+	5: "May",
+	6: "Jun",
+	7: "Jul",
+	8: "Aug",
+	9: "Sep",
+	10: "Oct",
+	11: "Nov",
+	12: "Dec"
+}
+
+## Emite when the thread is released.
+signal thread_released()
 
 var http_server = self
 
-class SortPoid:
-	static func sort_ascending(a, b) -> bool:
-		if a[1] < b[1]:
-			return true
-		return false
+static func sort_ascending(a, b) -> bool:
+	if a[1] < b[1]:
+		return true
+	return false
 
 
 func get_data_priority_header(header) -> Array:
@@ -57,10 +60,10 @@ func get_data_priority_header(header) -> Array:
 	for gf in header.replace(" ", "").split(","):
 		var ggffd = gf.split(";q=")
 		
-		if !ggffd.empty() and ggffd.size() < 3:
+		if not ggffd.is_empty() and ggffd.size() < 3:
 			if ggffd.size() == 1:
 				ggffd.insert(1, "1")
-			elif !ggffd[1].is_valid_float():
+			elif not ggffd[1].is_valid_float():
 				#invalide, on renvoi rien
 				return []
 		else:
@@ -69,18 +72,19 @@ func get_data_priority_header(header) -> Array:
 			
 		result.push_back(ggffd)
 	
-	result.sort_custom(SortPoid, "sort_ascending")
+	result.sort_custom(Callable(self, "sort_ascending"))
 	
 	return result
 
-# lire le contenu (données, fichiers ect envoyés par le client)
+
+#lire le contenue (datas, fichiers ect envoyer pas le client)
 class FieldStorage:
 	var form_data = {
 		"mime": {},
 		"champs": {}
 	}
 	
-	func _init(environ) -> void:
+	func _init(environ):
 		var content_type = environ["CONTENT-TYPE"].split("; ")
 		
 		form_data["mime"]["type"] = content_type[0]
@@ -88,14 +92,14 @@ class FieldStorage:
 		
 		if content_type[0] == "application/x-www-form-urlencoded":
 			for key_value in environ["CONTENT"].split("&"):
-				form_data[key_value.split("=")[0].http_unescape()] = key_value.split("=")[1].http_unescape()
+				form_data[key_value.split("=")[0].uri_decode()] = key_value.split("=")[1].uri_decode()
 		elif content_type[0] == "multipart/form-data":
 			var boundary = content_type[1].replace("boundary=", "")
 			
 			#Si la fin du message multipart/form-data est valide
 			if environ["CONTENT"].ends_with("--" + boundary + "--"):
-				var form: PoolStringArray = environ["CONTENT"].split("--" + boundary, false)
-				#Nous supprimons le premier élément du tableau car il est vide.
+				var form: PackedStringArray = environ["CONTENT"].split("--" + boundary, false)
+				#On supprime le premier élément du tableau car il est vide.
 				form.resize(form.size() - 1)
 				
 				var regex = RegEx.new()
@@ -118,7 +122,7 @@ var ssl_certificate: X509Certificate
 ##When using SSL (see private_key and ssl_certificate), you can set this to a valid X509Certificate to be provided as additional CA chain information during the SSL handshake.
 var ca_chain: X509Certificate
 
-var server := TCP_Server.new()
+var tcp_server := TCPServer.new()
 var mutex := Mutex.new()
 
 ##If [true] the verboss mode is actived.
@@ -136,11 +140,9 @@ var accept_compress := false
 
 var ssl := false
 
-# application helloworld par défaut
-func _application(environ):
-	return ["200 OK", {"content-type": "text/plain"}, "Hello world".to_utf8()]
-
-var application := funcref(self, "_application")
+# application helloworld par deffaut
+var application := func _application(environ):
+	return ["200 OK", {"content-type": "text/plain"}, "Hello world".to_utf8_buffer()]
 
 var application_error
 
@@ -162,7 +164,7 @@ func http_error(environ: Dictionary, status_code: int):
 			http_status = "503 Service Unavailable"
 	
 	if application_error:
-		var data_vus = application_error.call_func(environ, status_code)
+		var data_vus = application_error.call(environ, status_code)
 		
 		if typeof(data_vus) != TYPE_ARRAY:
 			push_error("(HTTPServer) The function assigned to the \"application_error\" variable of the server must return an \"Array\" type object.")
@@ -173,12 +175,12 @@ func http_error(environ: Dictionary, status_code: int):
 		else:
 			return [http_status] + data_vus
 	
-	return [http_status, {"content-type": "text/plain"}, http_status.to_utf8()]
+	return [http_status, {"content-type": "text/plain"}, http_status.to_utf8_buffer()]
 
 
-##Starts listening on the given port.
+## Starts listening on the given port.
 func listen(port: int = 80, bind_address: String = "*") -> int:
-	var error = server.listen(port)
+	var error = tcp_server.listen(port)
 	
 	if verboss_mode:
 		if error == OK:
@@ -189,30 +191,28 @@ func listen(port: int = 80, bind_address: String = "*") -> int:
 	return error
 
 
-##Stop the server and clear its state.
+## Stop the server and clear its state.
 func stop() -> void:
-	server.stop()
+	tcp_server.stop()
 	
 	if verboss_mode:
 		print("The server is no longer listening for connections.")
 
 
-##Return [true] if the server is currently listening for connections.
+## Return [true] if the server is currently listening for connections.
 func is_listening() -> bool:
-	return server.is_listening()
+	return tcp_server.is_listening()
 
-##This needs to be called in order to have any request processed.
+## This needs to be called in order to have any request processed.
 var threads = {}
 
 var semaphores = {}
 
-func poll():
-	if server.is_connection_available():
-		var peer = server.take_connection()
+func poll() -> void:
+	if tcp_server.is_connection_available():
+		var peer = tcp_server.take_connection()
 		
 		if peer.is_connected_to_host():
-			
-			#test
 			if threads.size() < max_worker or max_worker < 0:
 				var thread := Thread.new()
 				var semaphore = Semaphore.new()
@@ -222,7 +222,7 @@ func poll():
 				
 				threads[thread] = []
 			
-			# nous recherchons un thread qui a le moins de clients à gérer
+			# On cherche un tread qui a le moins de client a gérer
 			var worker_libre = [null, 500]
 			for thread in threads.keys():
 				if threads[thread].size() < worker_libre[1]:
@@ -245,13 +245,14 @@ func poll():
 				threads[worker_libre[0]].append(StreamPeerHTTP.new(settings))
 			else:
 				print("max_client_per_worker attin!")
-			
-	#nous détruisons les threads qui n'ont plus de clients à gérer
+	
+	# On tue les thread qui non plus de clients a gèrer
 	for thread in threads.keys():
-		if threads[thread].empty():
+		if threads[thread].is_empty():
 			threads.erase(thread)
 			semaphores.erase(thread)
-			print("thread dead!")
+			
+			emit_signal("thread_released")
 	
 	for thread in semaphores.keys():
 		semaphores[thread].post()
@@ -262,21 +263,21 @@ func _traitement(settings):
 	while true:
 		settings.semaphore.wait()
 		
-		for stream_peer_HTTP in threads[settings.thread]:
-			if stream_peer_HTTP.process():
-				#si le peer demende a mourir
+		for stream_peer_http in threads[settings.thread]:
+			if stream_peer_http.process():
+				# Si le peer demende a mourir
 				settings.mutex.lock()
-				threads[settings.thread].remove(threads[settings.thread].find(stream_peer_HTTP))
+				threads[settings.thread].remove(threads[settings.thread].find(stream_peer_http))
 				settings.mutex.unlock()
 #fin test
 
 
 class StreamPeerHTTP:
-	signal kill_StreamPeerHTTP(StreamPeerHTTP)
+	signal stream_peer_http_killed(stream_peer_http)
 	
 	var settings
 	var peer
-	var timeout
+	var timeout: int
 	var http_response := ""
 	var data_vus: Array
 	var application_error
@@ -300,7 +301,7 @@ class StreamPeerHTTP:
 			
 			peer.accept_stream(settings.peer, settings.private_key, settings.ssl_certificate, settings.ca_chain)
 		
-		timeout = settings.keep_alive_timeout + OS.get_system_time_secs()
+		timeout = settings.keep_alive_timeout + Time.get_unix_time_from_system()
 	
 	func http_error(environ: Dictionary, status_code: int):
 		var http_status
@@ -320,7 +321,7 @@ class StreamPeerHTTP:
 				http_status = "503 Service Unavailable"
 		
 		if application_error:
-			var data_vus = application_error.call_func(environ, status_code)
+			var data_vus = application_error.call(environ, status_code)
 			
 			if typeof(data_vus) != TYPE_ARRAY:
 				push_error("(HTTPServer) The function assigned to the \"application_error\" variable of the server must return an \"Array\" type object.")
@@ -331,7 +332,7 @@ class StreamPeerHTTP:
 			else:
 				return [http_status] + data_vus
 		
-		return [http_status, {"content-type": "text/plain"}, http_status.to_utf8()]
+		return [http_status, {"content-type": "text/plain"}, http_status.to_utf8_buffer()]
 		
 	func get_data_priority_header(header) -> Array:
 		var result = []
@@ -339,19 +340,19 @@ class StreamPeerHTTP:
 		for gf in header.replace(" ", "").split(","):
 			var ggffd = gf.split(";q=")
 			
-			if !ggffd.empty() and ggffd.size() < 3:
+			if not ggffd.is_empty() and ggffd.size() < 3:
 				if ggffd.size() == 1:
 					ggffd.insert(1, "1")
-				elif !ggffd[1].is_valid_float():
-					#invalide, on renvoi rien
+				elif not ggffd[1].is_valid_float():
+					# invalide, on renvoi rien
 					return []
 			else:
-				#invalide, on renvoi rien
+				# invalide, on renvoi rien
 				return []
 				
 			result.push_back(ggffd)
 		
-		result.sort_custom(SortPoid, "sort_ascending")
+		result.sort_custom(Callable(self, "sort_ascending"))
 		
 		return result
 	
@@ -360,16 +361,16 @@ class StreamPeerHTTP:
 			target[key] = patch[key]
 	
 	func process():
-		if((settings.keep_alive and OS.get_system_time_secs() < timeout) or !settings.keep_alive or header) and settings.peer.is_connected_to_host():
+		if((settings.keep_alive and Time.get_unix_time_from_system() < timeout) or not settings.keep_alive or header) and settings.peer.is_connected_to_host():
 			if peer.get_status() == StreamPeerSSL.STATUS_CONNECTED or peer.get_status() == StreamPeerSSL.STATUS_HANDSHAKING:
-				if !header:
+				if not header:
 					if ssl:
 						peer.poll()
 					
-					#on récup la requet du client
+					# On récup la requet du client
 					var response = peer.get_partial_data(peer.get_available_bytes())
 					
-					#si il y a pas d'erreur
+					# Si ya pas d'erreur
 					if response[0] == OK:
 						http_response += response[1].get_string_from_utf8()
 					else:
@@ -384,7 +385,7 @@ class StreamPeerHTTP:
 					if http_response.ends_with("\r\n"):
 						numb += 1
 						if settings.keep_alive:
-							timeout = settings.keep_alive_timeout + OS.get_system_time_secs()
+							timeout = settings.keep_alive_timeout + Time.get_unix_time_from_system()
 						
 						var headers_dictionary = {
 							"accept-ranges": "bytes",
@@ -396,17 +397,17 @@ class StreamPeerHTTP:
 							"REMOTE_PORT": peer.get_connected_port()
 						}
 						
-						#on valide la request du client
-						var regex = RegEx.new()
+						# On valide la request du client
+						var regex := RegEx.new()
 						
 						regex.compile("^(GET|HEAD|POST|OPTIONS|CONNECT|TRACE|PUT|PATCH|DELETE) (/(?:\\S[^#\\s\\?\\x80-\\xFF]*)?)(\\?[^:#\\s\\x80-\\xFF]*)? HTTP/(\\d+)(\\.\\d+)?\\r\\n((?:(?:[ -~])+\\r\\n)+)\\r\\n(?:((?:.|\\s)*)\\r\\n|)")
 						
-						var result = regex.search(http_response)
+						var result := regex.search(http_response)
 						
 						if result:
 							environ["REQUEST_METHOD"] = result.strings[1]
-							environ["PATH"] = result.strings[2].http_unescape()
-							environ["QUERY_STRING"] = result.strings[3].http_unescape()
+							environ["PATH"] = result.strings[2].uri_decode()
+							environ["QUERY_STRING"] = result.strings[3].uri_decode()
 							environ["SERVER_PROTOCOL"] = result.strings[4] + result.strings[5]
 							environ["CONTENT"] = result.strings[7]
 							
@@ -424,43 +425,43 @@ class StreamPeerHTTP:
 									environ = {}
 									break
 							
-							if environ["SERVER_PROTOCOL"] == "HTTP/1.1" and !"HOST" in environ:
+							if environ["SERVER_PROTOCOL"] == "HTTP/1.1" and not "HOST" in environ:
 								environ = {}
 						else:
 							print("erreur, 400")
 						
 						if response[0] == OK:
 							if environ:
-								data_vus = application.call_funcv([environ])
+								data_vus = application.call(environ)
 								
-								#on pence a vérifier la valeur de retour par la fonction assigner la variable d'application serveur
+								# On pence a vérif les valeur retourner parle la fonction affecter la la variable aplication du server.
 								if typeof(data_vus) != TYPE_ARRAY:
-									push_error("(HTTPServer) The function assigned to the \"application\" variable of the server must return an \"Array\" type object.")
+									print("(HTTPServer) The function assigned to the \"application\" variable of the server must return an \"Array\" type object.")
 									data_vus = http_error(environ, 500)
 								elif typeof(data_vus[0]) != TYPE_STRING:
-									push_error("(HTTPServer) The object at index 0 of the \"Array\" object returned by the function assigned to the \"application\" variable of the server must be of type \"String\".")
+									print("(HTTPServer) The object at index 0 of the \"Array\" object returned by the function assigned to the \"application\" variable of the server must be of type \"String\".")
 									data_vus = http_error(environ, 500)
 								elif typeof(data_vus[1]) != TYPE_DICTIONARY:
-									push_error("(HTTPServer) The object at index 1 of the \"Array\" object returned by the function assigned to the \"application\" variable of the server must be of type \"Dictionary\".")
+									print("(HTTPServer) The object at index 1 of the \"Array\" object returned by the function assigned to the \"application\" variable of the server must be of type \"Dictionary\".")
 									data_vus = http_error(environ, 500)
 								elif typeof(data_vus[2]) != TYPE_RAW_ARRAY:
-									push_error("(HTTPServer) The object at index 2 of the \"Array\" object returned by the function assigned to the \"application\" variable of the server must be of type \"PoolByteArray\".")
+									print("(HTTPServer) The object at index 2 of the \"Array\" object returned by the function assigned to the \"application\" variable of the server must be of type \"PoolByteArray\".")
 									data_vus = http_error(environ, 500)
-									
+								
 								if "RANGE" in environ and environ["REQUEST_METHOD"] == "GET":
-									var regeTTTTx = RegEx.new()
-									
-									regeTTTTx.compile("^bytes=(\\d+)-(\\d*)")
-									var resultTTT = regeTTTTx.search(environ["RANGE"])
+									regex.compile("^bytes=(\\d+)-(\\d*)")
+									var resultTTT = regex.search(environ["RANGE"])
 									
 									if resultTTT:
 										data_vus[0] = "206 Partial Content"
 										
 										var to = resultTTT.strings[2]
-										if !to:
+										
+										if to.is_empty():
 											to = data_vus[2].size() - 1
 										
 										headers_dictionary["content-range"] = "bytes %s-%d/%d" % [resultTTT.strings[1], to, data_vus[2].size()]
+										
 										data_vus[2] = data_vus[2].subarray(resultTTT.strings[1], to)
 									else:
 										data_vus = http_error(environ, 400)
@@ -490,12 +491,12 @@ class StreamPeerHTTP:
 						
 						dictionary_merge(headers_dictionary, data_vus[1])
 						
-						var datetime = OS.get_datetime(true)
+						var datetime = Time.get_datetime_dict_from_system(true)
 						
 						var datetime_datas = [
-							DATETIM_NAME.day[datetime.weekday],
+							DAYS_NAME[datetime.weekday],
 							str(datetime.day).pad_zeros(2),
-							DATETIM_NAME.month[datetime.month],
+							MONTHS_NAME[datetime.month],
 							datetime.year,
 							str(datetime.hour).pad_zeros(2),
 							str(datetime.minute).pad_zeros(2),
@@ -515,13 +516,13 @@ class StreamPeerHTTP:
 						for field_name in headers_dictionary.keys():
 							header = header + field_name + ": " + headers_dictionary[field_name] + "\r\n"
 						
-						header = (header + "\r\n").to_utf8()
+						header = (header + "\r\n").to_utf8_buffer()
 						
 						if environ["REQUEST_METHOD"] != "HEAD":
 							header.append_array(data_vus[2])
 						
 						if verboss_mode:
-							prints(environ["REMOTE_ADDR"] + ":" + String(environ["REMOTE_PORT"]), environ["PATH"], environ["REQUEST_METHOD"], data_vus[0])
+							prints(environ["REMOTE_ADDR"] + ":" + str(environ["REMOTE_PORT"]), environ["PATH"], environ["REQUEST_METHOD"], data_vus[0])
 				
 				if header:
 					var error_and_cursor_responce = peer.put_partial_data(header)
@@ -531,15 +532,18 @@ class StreamPeerHTTP:
 						
 						header = null
 					else:
-						#on tronque la portion servie par le serveur pour garder que la portion inutilisée qui devra plus tard être servie par le serveur
-						header = header.subarray(error_and_cursor_responce[1], -1)
-						
-					#lorsque toute la réponse a été servie
-					if !header:
+						# On tronque la parti servi par le serveur pour garder que la parti non servi qui plustard devra être servi par le serveur.
+						if header.size() != error_and_cursor_responce[1]:
+							header = header.subarray(error_and_cursor_responce[1], -1)
+						else:
+							header = null
+					
+					# Quand la totalité de la réponce a été servie.
+					if not header:
 						data_vus = []
 						print("complet")
 						http_response = ""
-						if !settings.keep_alive:
+						if not settings.keep_alive:
 							if ssl:
 								peer.disconnect_from_stream()
 							else:
